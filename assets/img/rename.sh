9ce3cf9e39aed8af3a42c344f8d5a74e3f655de9
Bartek Skorulski <bartekskorulski@gmail.com>
2018-05-07 22:15:30 +0200

    adds new photos

--- assets/img/rename.sh
+++ assets/img/rename.sh
@@ -9,7 +9,7 @@ mkdir thumbnails
 for i in $(ls *.jpg); do convert -scale 100x100 $i thumbnails/$i; done
 
 for i in *.jpg; do
-  printf "<a href='assets/img/C15/$i' data-gallery>\n"
-  printf "  <img src='assets/img/C15/thumbnails/$i'>\n"
+  printf "<a href='assets/img/C16/$i' data-gallery>\n"
+  printf "  <img src='assets/img/C16/thumbnails/$i'>\n"
   echo "</a>"
 done
